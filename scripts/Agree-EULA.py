cdd2d6dc77967b11e84a5978b7484648f7d6747e
zehuajun <zehuajun@zehuajun.top>
2024-06-14 14:20:22 +0800

    添加同意 EULA 的脚本

---
+++ scripts/Agree-EULA.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+这个脚本用于自动同意 Minecraft EULA
+Author: ZeHuaJun
+Created on: 2024-06-14
+"""
+
+import re
+
+f=open('server/eula.txt','r')
+alllines=f.readlines()
+f.close()
+f=open('server/eula.txt','w+')
+for eachline in alllines:
+        a=re.sub('false','true',eachline)
+f.writelines(a)
+f.close()
